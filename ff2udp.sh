d106125640e8f3fac7b758c248b159658e192b57
iamhyc <sudofree@163.com>
2019-09-06 20:32:44 +0800

    init

---
+++ ff2udp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ADDR=127.0.0.1
+if [ -n $1 ]; then
+    ADDR=$1;
+fi
+
+ffmpeg -f v4l2 -i "/dev/video0" \
+-profile:v high -pix_fmt yuvj420p -level:v 4.1 -preset ultrafast -tune zerolatency \
+-vcodec libx264 -r 30 -b:v 512k -s 640x360 \
+-f mpegts -flush_packets 0 udp://$ADDR:1234?pkt_size=1316
